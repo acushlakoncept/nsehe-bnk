818039d082f3f54cf2fec17c30bfe5f6945eb214
Uduak Essien <acushla4real@gmail.com>
2021-08-16 00:09:33 +0100

    Add method to generate account

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,10 @@
 class Account < ApplicationRecord
   belongs_to :user
+  before_save :account_number
+
+  private
+
+  def account_number
+    self.account_number = ('%010d' % rand(0..9999999999)).to_i
+  end
 end
