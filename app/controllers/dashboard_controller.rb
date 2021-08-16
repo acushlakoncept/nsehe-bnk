8ce2c9cff56458198930e359a3a78d27c6a79772
Uduak Essien <acushla4real@gmail.com>
2021-08-16 15:27:26 +0100

    Add deposit, transaction, transfer, withdraw page

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,19 @@
 class DashboardController < ApplicationController
-    before_action :authenticate_user!
-    layout "dashboard"
-    
-    def index
-    end
+  before_action :authenticate_user!
+  layout "dashboard"
+      
+  def index
+  end
+
+  def deposit
+  end
+
+  def withdraw
+  end
+
+  def transfer
+  end
+
+  def transaction
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :users
   root to: "dashboard#index"
+  get "deposit", to: "dashboard#deposit"
+  get "withdraw", to: "dashboard#withdraw"
+  get "transfer", to: "dashboard#transfer"
+  get "transaction", to: "dashboard#transaction"
 end
