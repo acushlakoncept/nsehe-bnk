7df8da43f279cd8411d132f98d71ab750d492698
Uduak Essien <acushla4real@gmail.com>
2021-08-16 15:26:29 +0100

    Add user utility methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,33 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :trackable
 
   validates_presence_of :first_name, :last_name, :username, :country, :state, :city, :address, :security_question
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+  has_one :account
+
+  def has_account?
+    account ? true : false
+  end
+
+  def balance
+    "#{Currency.find(account.currency_id).symbol} #{account.balance}"
+  end
+
+  def account_number
+    account.account_number
+  end
+
+  def account_type
+    AccountType.find(account.account_type_id).name
+  end
+
+  def account_status
+    Status.find(account.status_id).name
+  end
+
+  def status_style
+    account_status == 'Active' ? 'success' : 'danger'
+  end
 end
