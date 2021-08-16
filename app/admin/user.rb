8640bc7112b89516258b3d278932a158f0338ada
Uduak Essien <acushla4real@gmail.com>
2021-08-16 15:26:49 +0100

    Add customize data for user

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,6 +1,7 @@
 ActiveAdmin.register User do
     menu priority: 1
-    permit_params :first_name, :last_name, :username, :email, :status, :phone_number
+    permit_params :first_name, :last_name, :username, :email, :status, :phone_number,
+    :country, :state, :city, :address, :security_question, :security_answer
 
     actions :all
 
@@ -10,8 +11,18 @@ ActiveAdmin.register User do
         column :last_name
         column :username
         column :email
+        column :country
         column :status
         column :created_at
         actions
+    end
+
+    form do |f|
+        f.input :first_name
+        f.input :last_name
+        f.input :username
+        f.input :email
+        f.input :country
+        f.actions
     end
 end
