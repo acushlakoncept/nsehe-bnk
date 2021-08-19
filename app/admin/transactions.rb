0969cebd6ce62b29e4515673334d9ce99e7d4687
Uduak Essien <acushla4real@gmail.com>
2021-08-19 05:49:58 +0100

    Fix rubocop error

--- app/admin/transactions.rb
+++ app/admin/transactions.rb
@@ -1,15 +1,15 @@
 ActiveAdmin.register Transaction do
-    permit_params :description, :debits, :credits, :user_id, :transaction_date
+  permit_params :description, :debits, :credits, :user_id, :transaction_date
 
-    actions :all
+  actions :all
 
-    index do
-        selectable_column
-        column :user
-        column :description
-        column :debits
-        column :credits
-        column :transaction_date
-        actions
-    end
+  index do
+    selectable_column
+    column :user
+    column :description
+    column :debits
+    column :credits
+    column :transaction_date
+    actions
+  end
 end
