9876f56824ae824fc753ac219817903d5f0f3d61
Uduak Essien <acushla4real@gmail.com>
2023-08-12 13:13:13 +0100

    Add currency symbol helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def currency_symbol(code)
+    symbols = {
+      'USD' => '$',
+      'EUR' => '€'
+      # Add more currency code and symbol mappings as needed
+    }
+    
+    symbols[code] || code
+  end
 end
