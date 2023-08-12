0b280f9f47e9e10df2037a58b9ec3206a612b588
Uduak Essien <acushla4real@gmail.com>
2023-08-12 13:12:52 +0100

    Update account to use enums

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,11 +1,29 @@
 class Account < ApplicationRecord
   belongs_to :user
-  belongs_to :status
-  belongs_to :account_type
-  belongs_to :currency
   before_create :gen_account_number
 
+  SAVINGS = 'Savings'.freeze
+  CURRENT = 'Current'.freeze
+
+  ACTIVE = 'Active'.freeze
+  INACTIVE = 'Inactive'.freeze
+
+  USD = 'USD'.freeze
+  EUR = 'EUR'.freeze
+
   validates! :account_number, uniqueness: true
+  enum account_type: { SAVINGS => 0, CURRENT => 1 }, _prefix: true
+  enum status: { ACTIVE => 0, INACTIVE => 1 }, _prefix: true
+  enum currency: { USD => 0, EUR => 1 }, _prefix: true
+
+  def activate
+    bybug
+    if account.nil?
+      Account.create(account_type: 'Savings', currency: 'USD', status: 'Active')
+    else
+      account.update(account_type: 'Savings', currency: 'USD', status: 'Active')
+    end
+  end
 
   private
 
@@ -15,4 +33,5 @@ class Account < ApplicationRecord
       break number unless Account.exists?(account_number: number)
     end
   end
+
 end
